f3dcaae272d2b1d139e15d9ac7c600dbf464663d
unknown <natvue@icloud.com>
2024-02-08 22:01:27 -0600

    Added buttons and functions for GUI

--- 2.2.7.py
+++ 2.2.7.py
@@ -7,17 +7,90 @@ import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 
-def do_command():
-    subprocess.call("ping localhost")
+# Modify the do_command function:
+#   to use the new button as needed
+def do_command(command):
+    global command_textbox, url_entry
+    
+    # If url_entry is blank, use localhost IP address 
+    url_val = url_entry.get()
+    if (len(url_val) == 0):
+        # url_val = "127.0.0.1"
+        url_val = "::1"
+    print(url_val + " url nat")
+
+    command_textbox.insert(tk.END, command + " working....\n")
+    command_textbox.update()
+
+    p = subprocess.Popen(command + ' ::1', stdout=subprocess.PIPE, stderr=subprocess.PIPE) #v2
+
+    cmd_results, cmd_errors = p.communicate()
+    command_textbox.insert(tk.END, cmd_results)
+    command_textbox.insert(tk.END, cmd_errors)
+
+# Save function.
+def mSave():
+  filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
+  if filename is None:
+    return
+  file = open (filename, mode = 'w')
+  text_to_save = command_textbox.get("1.0", tk.END)
+  
+  file.write(text_to_save)
+  file.close()
 
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
-# set up button to run the do_command function
-ping_btn = tk.Button(frame, text="ping", command=do_command)
-ping_btn.pack()
-
+# set up buttons to run the do_command function
+# Makes the command button pass it's name to a function using lambda
+ping_btn = tk.Button(
+   frame,
+   text="Ping",
+   command=lambda:do_command("ping"),
+   compound="center",
+    font=("comic sans", 12),
+    bd=0, 
+    relief="flat",
+    cursor="heart",
+    # image=img, 
+    bg="lightblue",
+    activebackground="gray"
+   )
+ping_btn.pack(side="left")
+tracert_btn = tk.Button(
+   frame, text="Tracert", 
+   command=lambda:do_command("tracert"),
+   compound="center",
+    font=("comic sans", 12),
+    bd=0, 
+    relief="flat",
+    cursor="heart",
+    # image=img, 
+    bg="lightblue",
+    activebackground="gray")
+tracert_btn.pack(side="left")
+nslookup_btn = tk.Button(frame, text="Nslookup", command=lambda:do_command("nslookup"),
+   compound="center",
+    font=("comic sans", 12),
+    bd=0, 
+    relief="flat",
+    cursor="heart",
+    # image=img, 
+    bg="lightblue",
+    activebackground="gray")
+nslookup_btn.pack(side="left")
+save_btn = tk.Button(frame, text="Save", command=lambda:mSave(),
+   compound="center",
+    font=("comic sans", 12),
+    bd=0, 
+    relief="flat",
+    cursor="heart",
+    # image=img, 
+    bg="lightblue",
+    activebackground="gray")
+save_btn.pack(side="left")
 # creates the frame with label for the text box
 frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
 frame_URL.pack()
